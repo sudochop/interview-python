9670282b00ef70692320541ea756c5a9063fba48
Michael Miller <mmiller.6@go.ccad.edu>
2020-08-20 12:39:54 -0400

    Approx 1 hour to complete

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 def decode_auth_token(auth_token):
     # use jwt, jwt_secret_key
     # should be a one liner, but we want you to see how JWTs work
-    pass
+    return jwt.decode(auth_token, jwt_secret_key)
 
 
 def encode_auth_token(user_id, name, email, scopes):
@@ -28,8 +28,12 @@ def encode_auth_token(user_id, name, email, scopes):
         'name': name,
         'email': email,
         'scope': scopes,
-        'exp': mktime((datetime.datetime.now() + datetime.timedelta(days=1)).timetuple())
+        'exp': mktime(
+            (datetime.datetime.now() + datetime.timedelta(days=1)).timetuple()
+        ),
     }
+
+    return jwt.encode(payload, jwt_secret_key).decode('UTF-8')
 
 
 def get_user_from_token():
@@ -37,22 +41,43 @@ def get_user_from_token():
     # should pull token from the Authorization header
     # Authorization: Bearer {token}
     # Where {token} is the token created by the login route
-    pass
+    _, token = request.headers.get('Authorization', '').split()
+    return decode_auth_token(token)
+
+
+def filter_widget(widget, widget_type, created_start, created_end):
+    if widget_type is not None and widget['type'] != widget_type:
+        return False
+
+    created = parse_date_time(widget['created'])
+
+    if created_start is not None and created_start > created:
+        return False
+    if created_end is not None and created > created_end:
+        return False
+
+    return True
+
+
+def format_widget(widget):
+    return {
+        'id': widget['id'],
+        'type': widget['type'],
+        'type_label': widget['type'].replace('-', ' ').title(),
+        'created': widget['created'],
+    }
 
 
 @app.route('/')
 def status():
-    return 'API Is Up'
+    return 'API Is Up\n'
 
 
 @app.route('/user', methods=['GET'])
 def user():
     # get the user data from the auth/header/jwt
-    return {
-        'user_id': '',
-        'name': '',
-        'email': ''
-    }
+    user = get_user_from_token()
+    return {'user_id': user['sub'], 'name': user['name'], 'email': user['email']}
 
 
 @app.route('/login', methods=['POST'])
@@ -61,9 +86,16 @@ def login():
     # use the get_user_by_email function to get the user data
     # return a the encoded json web token as a token property on the json response as in the format below
     # we're not actually validitating a password or anything because that would add unneeded complexity
-    return {
-        'token': ''
-    }
+    body = request.get_json()
+    email = body.get('email')
+    scopes = body.get('scopes')
+
+    # I Would normaly use something like flask-inputs here.
+    # For brevity I'm going to assume the correct data is provided.
+
+    user = get_user_by_email(email)
+
+    return {'token': encode_auth_token(user['id'], user['name'], user['email'], scopes)}
 
 
 @app.route('/widgets', methods=['GET'])
@@ -88,17 +120,32 @@ def widgets():
 
     # filter the results by the query parameters
     # return the data in the format below
+    token = get_user_from_token()
+
+    if 'widgets' not in token.get('scope', []):
+        return 'Unauthorized\n', 401
+
+    r = requests.get(
+        'https://us-central1-interview-d93bf.cloudfunctions.net/widgets',
+        headers={'Authorization': f'apiKey {api_auth_token}'},
+        params={'user_id': token['sub']},
+    )
+    response = r.json()
+
+    widget_type = request.args.get('type')
+    start = request.args.get('created_start')
+    end = request.args.get('created_end')
+
+    start = parse_date_time(start) if start is not None else None
+    end = parse_date_time(end) if end is not None else None
+
+    # Could be optimized by checking if any query params at all first.
+    matching_items = filter(lambda w: filter_widget(w, widget_type, start, end), response)
+    matching_items = map(format_widget, matching_items)
 
     return {
-        'total_widgets_own_by_user': 2,
-        'matching_items': [
-            {
-                "id": 0,
-                "type": "foo-bar",
-                "type_label": "Foo Bar",  # replace dashes with spaces and capitalize words
-                "created": datetime.datetime.now().isoformat(), # remember to replace
-            }
-        ]
+        'total_widgets_own_by_user': len(response),
+        'matching_items': list(matching_items),
     }
 
 
